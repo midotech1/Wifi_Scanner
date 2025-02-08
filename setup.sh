c1e948f57c81e9379fa42f1dc5e12d6b25eb1dd9
midotech1 <midoshop666@gmail.com>
2025-02-09 00:30:56 +0100

    Create setup.sh
    Setup file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# Requirements of tool 
+echo "Start Setup Tool..."
+pip install scapy
+pip install pyfiglet
+echo "Setup finished..."
+echo "NOTICE: Please run this program with a monitor mode enabled !!"
